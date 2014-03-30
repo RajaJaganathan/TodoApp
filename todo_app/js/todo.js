0d65e71876f8f28ecfed2b30d983c3169f85d356
Raja <rajajflex@gmail.com>
2014-03-30 13:31:38 +0530

    initial commit todo sample app

---
+++ todo_app/js/todo.js
@@ -0,0 +1,42 @@
+﻿//@author Raja Jaganathan
+
+//Todo Application
+
+var Todo = Todo || (function () {
+
+    return {
+
+        addTask: function () {
+            if ($("#txtTask").val().length <= 0)
+                return;
+
+            $("#task-template").tmpl({ task: $("#txtTask").val() }).appendTo(".todoList");
+            $("#txtTask").val("");
+        },
+
+        init: function () {
+
+            $("#addTaskButton").on("click", function () {
+                console.log("addTaskButton clicked");
+               Todo.addTask();
+            });
+
+            $(document).on("click", ".todoList li", function () {
+
+                $(this).toggleClass("taskCompleted");
+
+                var isTaskCompleted = $(this).hasClass("taskCompleted");
+
+                if (isTaskCompleted)
+                    $(this).find(".todoCheckbox").prop("checked", true);
+                else
+                    $(this).find(".todoCheckbox").prop("checked", false);
+            });
+        }
+    };
+
+})();
+
+$(function () {
+    Todo.init();
+});
